418e1c79e5e8d10175e20116049b2e13dde494e4
Yu-Hsi Chiang <arbuz.tw@gmail.com>
2025-06-27 00:53:11 +0900

    Add icons to language switcher
    Adds a language icon and a dropdown arrow to the language switcher to improve user experience.

--- components/LanguageSwitcher.vue
+++ components/LanguageSwitcher.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { LanguageIcon, ChevronDownIcon } from '@heroicons/vue/24/solid'
 const { locale, locales, setLocale } = useI18n();
 
 const availableLocales = computed(() => {
@@ -12,10 +13,11 @@ const switchLocale = (lang) => {
 
 <template>
   <div class="relative">
+    <LanguageIcon class="absolute top-1/2 left-2 -translate-y-1/2 w-5 h-5 text-white pointer-events-none" />
     <select
       v-model="locale"
       @change="switchLocale($event.target.value)"
-      class="bg-emerald-600 text-white cursor-pointer"
+      class="bg-emerald-600 text-white cursor-pointer pl-8 pr-8 appearance-none"
     >
       <option
         v-for="lang in availableLocales"
@@ -25,5 +27,6 @@ const switchLocale = (lang) => {
         {{ lang.name }}
       </option>
     </select>
+    <ChevronDownIcon class="absolute top-1/2 right-2 -translate-y-1/2 w-5 h-5 text-white pointer-events-none" />
   </div>
 </template>
